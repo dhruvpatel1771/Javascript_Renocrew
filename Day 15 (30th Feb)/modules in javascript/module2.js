77358b02023e733d3ebc8fe134c2fb752dcf783c
dhruvpatel1771 <94777529+dhruvpatel1771@users.noreply.github.com>
2024-03-16 20:17:38 +0530

    Add files via upload

---
+++ Day 15 (30th Feb)/modules in javascript/module2.js
@@ -0,0 +1,15 @@
+// ES6 Modules
+export const hello = ()=>{
+    console.log("Hello Dhruv")
+}
+
+export const ahello = (name)=>{
+    console.log("Hello " + name)
+}
+
+const dhruv = ()=>{
+    console.log("Hello " + "Dhruv")
+}
+
+export default dhruv;
+ 
---
+++ Day 15 (30th Feb)/modules in javascript/modules.js
@@ -0,0 +1,8 @@
+// const {hello, ahello} = require("./module1") // commonjs
+import dhruv, {hello, ahello} from "./module2.js";
+hello()
+ahello("Shivani")
+ahello("Garima")
+ahello("Nitika")
+ahello("Aishwarya")
+dhruv()
